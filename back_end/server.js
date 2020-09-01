2b8220348dd2c2d6e8c8bd72778a75326d52f7c3
nicolendoruiz <nicolendoruiz@gmail.com>
2020-08-31 21:12:21 -0500

    Server.js

--- back_end/server.js
+++ back_end/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', function (req, res) {
-    res.send('Hola');
+    res.send('Bienvenido');
 });
 
 server.listen(3000, () => {
